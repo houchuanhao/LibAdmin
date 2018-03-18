c4ea7547abd8d80822ece8b8b83db8f745621f1b
houchuanhao <houchuanhao@imudges.com>
2018-03-18 12:07:34 +0800

    修改了button按钮的样式并重新生成了apk

--- app/src/main/java/com/libadmin/CorrectActivity.java
+++ app/src/main/java/com/libadmin/CorrectActivity.java
@@ -6,14 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import Utils.SubmitButton;
+
 public class CorrectActivity extends AppCompatActivity {
 
-    Button btn1,btn2;
+    Utils.SubmitButton btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_correct);
-        btn1=(Button)findViewById(R.id.inspection_btn1);
+        btn1=(SubmitButton)findViewById(R.id.inspection_btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/libadmin/MainActivity.java
+++ app/src/main/java/com/libadmin/MainActivity.java
@@ -7,20 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import Utils.SubmitButton;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-    private Button btn4;
+    private SubmitButton btn1;
+    private SubmitButton btn2;
+    private SubmitButton btn3;
+    private SubmitButton btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1=(Button)findViewById(R.id.main_btn1);//图书管理
-        btn2=(Button)findViewById(R.id.main_btn2);//自动巡检
-        btn3=(Button)findViewById(R.id.main_btn3);//乱书归位
-        btn4=(Button)findViewById(R.id.main_btn4);//机器人设置
+        btn1=(SubmitButton)findViewById(R.id.main_btn1);//图书管理
+        btn2=(SubmitButton)findViewById(R.id.main_btn2);//自动巡检
+        btn3=(SubmitButton)findViewById(R.id.main_btn3);//乱书归位
+        btn4=(SubmitButton)findViewById(R.id.main_btn4);//机器人设置
         initBtn();
     }
     void initBtn(){
